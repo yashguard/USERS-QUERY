f7240f0c993f5fc1dcfc47e0128f12be0c11f400
Yash Guard <yashguard>
2023-07-27 11:46:35 +0530

    First query solved

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ server.get("/", async (req, res) => {
   res.status(200).send(users);
 });
 
-server.listen(8020, () => {
+server.post("/users", async (req, res) => {
+  // <================== First Query ==================>
+  // <================== Find all the female users ==================>
+  const userList = await user.find({
+    gender: "Female",
+  });
+  const users = await user
+    .find({
+      gender: "Female",
+    })
+    .count();
+  res.status(200).send(userList);
+  console.log(users);
+});
+
+server.listen(8010, () => {
   console.log("Server is running");
   connect();
 });
